083436d702027623076c62c226473aa9d2bde723
Chrisld50 <chrisld50@yahoo.com>
2022-08-28 19:52:02 -0500

    working on the localStorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,15 @@ var currentDay = moment().format("MMM Do, YYYY");
 $("#currentDay").text(currentDay);
 
 $(".saveBtn").on("click", function () {
-    var dateInfo = $(".information")
+    var textInfo = $(".textInfo");
+    var timeInfo = $(this).parent().attr("id");
+    localStorage.setItem("timeInfo", JSON.stringify(timeInfo));
+    localStorage.setItem("textInfo", JSON.stringify(textInfo));
 
-    localStorage.setitem(dateInfo);
+
+
+    
+
+
 })
+
