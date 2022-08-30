a769790eac8f82dba4e65877f1e28eb08a069ae0
Chrisld50 <chrisld50@yahoo.com>
2022-08-29 22:22:21 -0500

    Added localstorage set and get for textarea

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,15 +2,27 @@ var currentDay = moment().format("MMM Do, YYYY");
 $("#currentDay").text(currentDay);
 
 $(".saveBtn").on("click", function () {
-    var textInfo = $(".textInfo");
+    var textInfo = $(this).siblings(".description").val();
     var timeInfo = $(this).parent().attr("id");
-    localStorage.setItem("timeInfo", JSON.stringify(timeInfo));
-    localStorage.setItem("textInfo", JSON.stringify(textInfo));
+    localStorage.setItem(timeInfo, textInfo);
+    console.log(textInfo)
 
+});
 
-
+for (i = 9; i <= 17; i++) {
+    let data
+    if (i < 12) {
+         data = localStorage.getItem(i + "am")
+         $("#" + i + "am").children().eq(1).append(data)
+    } else if (i === 12){
+        data = localStorage.getItem(i + "pm")
+        $("#" + i + "pm").children().eq(1).append(data)
+    }
+    else {
+         data = localStorage.getItem((i - 12) + "pm")
+         $("#" + i + "pm").children().eq(1).append(data)
+    }
     
-
-
-})
+    
+}
 
