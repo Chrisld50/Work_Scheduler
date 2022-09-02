ec8261058ccd4e59d4189998fec48d82f206cbfe
Chrisld50 <chrisld50@yahoo.com>
2022-09-01 23:02:04 -0500

    Updated Readme, added a screenshot, added comments to html and javascript

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Work_Scheduler
+
+# Description 
+My motivation was to apply bootstrap, javascript and jquery to an html with css already created to make a functional work scheduler calender that changes colors based on past time, present time and future time. This works by saving what is written within the schedule in local storage and changes the colors of the columns based on the time of day utilizing a timeframe from 9 am to 5 pm. My challenges were around converting the time from Standard time to Military time so that my .moments would work correctly. There was also some challenges when it came to getting the local storage to work. I was able to get local storage to both set and get at the end and the colors to change correctly based on the time. 
+
+# Technologies
+HTML 
+CSS 
+Javascript 
+Jquery 
+Bootstrap
+Moment
+Google Font
+Fontawesome
+
+# Screenshot 
+![Alt text](./Image/Work%20Scheduler%20Image.png)
+
+# Link
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,16 @@
-var currentDay = moment().format("MMM Do, YYYY");
+var currentDay = moment().format("MMM Do, YYYY"); //This is the code to set up the current day utilizing moment.
 $("#currentDay").text(currentDay);
 
 $(".saveBtn").on("click", function () {
-    var textInfo = $(this).siblings(".description").val();
+    var textInfo = $(this).siblings(".description").val(); //the save button here saves the items on the textarea line to locastorage.
     var timeInfo = $(this).parent().attr("id");
     localStorage.setItem(timeInfo, textInfo);
     console.log(textInfo)
 
 });
 
-for (i = 9; i <= 17; i++) {
-    let data
+for (i = 9; i <= 17; i++) {  //here is where we get the stored items from the save button out of local storage. 
+    let data 
     if (i < 12) {
          data = localStorage.getItem(i + "am")
          $("#" + i + "am").children().eq(1).append(data)
@@ -18,28 +18,28 @@ for (i = 9; i <= 17; i++) {
         data = localStorage.getItem(i + "pm")
         $("#" + i + "pm").children().eq(1).append(data)
     }
-    else {
+    else { 
          data = localStorage.getItem((i - 12) + "pm")
          $("#" + i + "pm").children().eq(1).append(data)
     }
         
 };
 
-function trackTime(){
-    var currentHour = moment().hour(); //military time 
+function trackTime(){  
+    var currentHour = moment().hour(); // here is the beginning of the function to track the time, displaying military time in the variable of moment().hour() . 
     var timeBlockArray = $(".time-block");
 
     
-   for (let x = 0; x < timeBlockArray.length; x++) {
+   for (let x = 0; x < timeBlockArray.length; x++) {  //this code converts the standard time that I had used on the html to the military time above for the ids. below continues the code to convert it to military time. 
     const textareaEl = timeBlockArray[x].children[1];
     var currentBlockTime = timeBlockArray[x].getAttribute("id")
 
 
-    var standardTime = parseInt(currentBlockTime.replace(/\D/g, '' ))
+    var standardTime = parseInt(currentBlockTime.replace(/\D/g, '' )) 
         if (standardTime < 9) {
             standardTime += 12
        }
-       if (standardTime < currentHour){
+       if (standardTime < currentHour){ //here is the code to change the colors of the columns depending on the time of day. 
         textareaEl.classList.add("past");
        }
        if (standardTime === currentHour){
